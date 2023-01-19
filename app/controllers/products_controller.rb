3038ad10a386766d38781e62735ff4c1af4337cf
Ernesto Mejias <ernepazzo1212@gmail.com>
2023-01-19 16:37:50 -0500

    17

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,35 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path, notice: 'Product was successfully created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])    
+  end
+
+  def update
     
   end
+  
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :price)
+  end
+  
+  
   
 end
---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  test 'render a list of products' do
+    get products_path 
+
+    assert_response :success
+    assert_select '.product', 2
+  end
+
+  test 'render a detailed product page' do
+    get product_path(products(:one))
+
+    assert_response :success
+    assert_select '.title', 'MyString1'
+    assert_select '.description', 'MyText1'
+    assert_select '.price', '1$'
+  end
+
+  test 'render a new product' do
+    get new_product_path
+
+    assert_response :success
+    assert_select 'form'
+  end
+
+  test 'allow to create a new product' do
+    post products_path, params: {
+      product: {
+        title: 'DDD',
+        description: 'dd dd dd',
+        price: 54
+      }
+    }
+
+    assert_redirected_to products_path
+    assert_equal flash[:notice], 'Product was successfully created'
+  end
+
+  test 'does not allow to create a new product with empty fields' do
+    post products_path, params: {
+      product: {
+        title: '',
+        description: 'dd dd dd',
+        price: 54
+      }
+    }
+
+    assert_response :unprocessable_entity
+  end
+
+  test 'render an edit product' do
+    get edit_product_path(products(:one))
+
+    assert_response :success
+    assert_select 'form'
+  end
+end
